c2145a2334ecbbaf35f45a1aca2932427e183955
hugo <hugomvera@gmail.com>
2022-04-17 10:50:22 -0700

    It is working now fully I just want it to commit

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
- path('', views.home),
+ path('', views.home,name = "home"),
+path("signup/", views.SignUp.as_view(), name="signup"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 from django.http import HttpResponse
@@ -10,3 +13,10 @@ from django.http import HttpResponse
 
 def home(request):
    return render(request, "users/home.html")
+
+
+class SignUp(CreateView):
+   form_class = UserCreationForm
+   success_url = reverse_lazy("login")
+   template_name = "registration/signup.html"
+
