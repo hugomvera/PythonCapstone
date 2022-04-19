adc2cfc8816f1715c3ca7afcb90455c4b219deb8
Tyler <tyduncanson@gmail.com>
2022-04-19 12:25:56 -0400

    Added Player page and link between that and homepage

--- users/views.py
+++ users/views.py
@@ -2,21 +2,26 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
+from .models import Player
 
 # Create your views here.
 from django.http import HttpResponse
 
-#def home(request):
-   #return render(request,"home.html")
-   #return HttpResponse('Home')
+# def home(request):
+# return render(request,"home.html")
+# return HttpResponse('Home')
 
 
 def home(request):
-   return render(request, "users/home.html")
+    return render(request, "users/home.html")
+
+
+def users(request):
+    users = Player.objects.all()
+    return render(request, "users.html", {"players": users})
 
 
 class SignUp(CreateView):
-   form_class = UserCreationForm
-   success_url = reverse_lazy("login")
-   template_name = "registration/signup.html"
-
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
