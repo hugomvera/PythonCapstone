39ef06331f0ff90f2598ad8766eae155ff3f7f5d
LornaGrajek <lorna.grajek@gmail.com>
2022-04-20 14:03:21 -0700

    html

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path("signup/", views.SignUp.as_view(), name="signup"),
-    path("users/", views.users)
+    path("users/", views.users, name="users")
 ]
