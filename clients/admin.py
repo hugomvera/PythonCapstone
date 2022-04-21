31ae3e453f2b385cc407914bed88bd61f4aefb5f
LornaGrajek <lorna.grajek@gmail.com>
2022-04-21 13:31:39 -0700

    added client pages

--- clients/admin.py
+++ clients/admin.py
@@ -1,34 +1,28 @@
 from django.contrib import admin
-from .models import Client
-from .models import Message
-from .models import Deposit
-from .models import Withdrawal
+from .models import  Client
+from .models import  Message
+from .models import  Deposit
+from .models import  Withdrawal
 
 # Register your models here.
-
-
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('code', 'description', 'time')
-
+    list_display = ('code', 'description','time')
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'cellPhone', 'email', 'image_url')
+    list_display = ('name','cellPhone','email','image_url')
+
 
 
 class DepositAdmin(admin.ModelAdmin):
-    list_display = ('depositID', 'applicationId', 'transactionAmount', 'transactionStatus',
-                    'transactionDate', 'datetime', 'transactionType', 'currencyCode')
+    list_display = ('depositID','applicationId','transactionAmount','transactionStatus','transactionDate','datetime','transactionType','currencyCode')
+
+admin.site.register(Client,ClientAdmin)
+admin.site.register(Message,MessageAdmin)
+admin.site.register(Deposit,DepositAdmin)
 
 
-admin.site.register(Client, ClientAdmin)
-admin.site.register(Message, MessageAdmin)
-admin.site.register(Deposit, DepositAdmin)
-
-
-# for withdrawals
+#########for withdrawals
 class WithdrawalAdmin(admin.ModelAdmin):
-    list_display = ('withdrawalID', 'applicationId', 'transactionAmount', 'transactionStatus',
-                    'transactionDate', 'datetime', 'transactionType', 'currencyCode')
+    list_display = ('withdrawalID','applicationId','transactionAmount','transactionStatus','transactionDate','datetime','transactionType','currencyCode')
 
-
-admin.site.register(Withdrawal, WithdrawalAdmin)
+admin.site.register(Withdrawal,WithdrawalAdmin)
