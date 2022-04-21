80968242d15b68694eca82c68f62a80a620172d6
Tyler <tyduncanson@gmail.com>
2022-04-21 12:09:57 -0400

    Added Styles and added more withdraw/deposit buttons

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1vwq&z#+$0f_uy(lx2#z(vi+%gd_4sdtgz31h%+&(q_93(th#r'
 
